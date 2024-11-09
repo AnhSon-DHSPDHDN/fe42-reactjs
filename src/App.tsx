111ae82ff48b07a59076427ba863b452333e9016
AnhSon-DHSPDHDN <anhson2121999@gmail.com>
2024-11-09 18:35:26 +0700

    base PJ

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import styles from "./App.module.css";
-
 function App() {
   return (
     <div className="App">
-      <img src="https://cdn.tuoitre.vn/thumb_w/480/2022/10/27/son-tung-4-1666838826176719239917.jpeg" />
-      <h1>SonTVA</h1>
-
-      <div className={styles.container}></div>
+      <h1>Hello World</h1>
     </div>
   );
 }
