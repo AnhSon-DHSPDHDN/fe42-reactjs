bf319eb0f1d4491d6b72e0687482efa6f9e4a862
AnhSon-DHSPDHDN <anhson2121999@gmail.com>
2024-11-21 18:35:02 +0700

    restful api react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  const fetchProfilesData = async () => {
+    const response = await axios.get("http://localhost:3001/profiles");
+    console.log(response.data, "response.data");
+  };
+
+  useEffect(() => {
+    fetchProfilesData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
